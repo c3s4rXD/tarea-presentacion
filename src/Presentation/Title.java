6d4acd1c22b963d61a77c8ccc16ee67a61385d7b
unknown <cesar.hincapie@correounivalle.edu.co>
2023-04-22 10:36:42 -0500

    entregando tarea de la presentacion

---
+++ src/Presentation/Title.java
@@ -0,0 +1,16 @@
+package Presentation;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Title extends JLabel{
+
+    public Title(String title, Color backgroundColor){
+        this.setText(title);
+        this.setBackground(backgroundColor);
+        this.setForeground(Color.orange);
+        this.setFont(new Font("Calibre",Font.BOLD+Font.ITALIC, 24));
+        this.setHorizontalAlignment(JLabel.CENTER);
+        this.setOpaque(true);
+    }
+}
